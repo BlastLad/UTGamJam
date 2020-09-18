b97d150a00581b71fe3e6bc31a33d43684b4f5c5
BlastLad <blastlad@gmail.com>
2020-09-18 13:01:30 -0400

    test for discord and the new tile palette

--- UTGameJam/Assets/Scripts/Player/SeanTestScripts/SeansTestPlayerController.cs
+++ UTGameJam/Assets/Scripts/Player/SeanTestScripts/SeansTestPlayerController.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class SeansTestPlayerController : MonoBehaviour
 {
+    [SerializeField]
+    private int playerSpeed = 5;
+    [SerializeField]
+    private float slowFallGravScale = 0.2f;
+    private float regularGravScale = 1f;
+
+    private bool isBarrierActive = false;
+
+
+
+    private Rigidbody2D rb;
+    private CircleCollider2D energyBarrierTrigger;
+
+    Vector3 startPosition;
+
+    Vector3 moveVector;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        energyBarrierTrigger = GetComponent<CircleCollider2D>();
+        startPosition = rb.transform.position;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +36,42 @@ public class SeansTestPlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveVector.x = Input.GetAxisRaw("Horizontal");
+        moveVector.y = 0;
+        moveVector.z = 0;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            transform.position = startPosition;
+        }
+
+        if (Input.GetMouseButton(1))
+        {
+            DeployBarrier();
+        }
+        else { RetractBarrier(); }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 movement = moveVector;
+        transform.position += moveVector * playerSpeed * Time.fixedDeltaTime;//updated position area
+        //rb.MovePosition(position);//actual movement of player
+    }
+
+
+    void DeployBarrier()
+    {
+        energyBarrierTrigger.enabled = true;
+        rb.gravityScale = slowFallGravScale;
+        isBarrierActive = true;
+    }
+
+    void RetractBarrier()
+    {
+        energyBarrierTrigger.enabled = false;
+        rb.gravityScale = regularGravScale;
+        isBarrierActive = false;
     }
 }
